926be0d79ffc2b72177a52232bb0a69757679a96
root <root@rp-f05769.ekelund.nu>
2019-03-13 17:13:30 +0000

    Backup logic

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ DIR=/media/mmcblk0p1/apps/sdr_transceiver_ft8
 
 chmod +x *.sh radioday upload-to-rbn
 
-rw
 cp temp.sh ~
 
 cp write-c2-files-day.cfg $DIR
 cp write-c2-files-night.cfg $DIR
 cp write-c2-files-gray.cfg $DIR
-
 cp radioday $DIR
 cp upload-to-rbn $DIR
 
-test `cat decode-ft8.sh | grep radioday | wc -l` || cp $DIR/decode-ft8.sh $DIR/decode-ft8.sh.orig
-cp decode-ft8.sh $DIR
-ro
-lbu commit -d
+# Back up original decode-ft8.sh 
+test -e $DIR/decode-ft8.sh.orig || \
+	cp $DIR/decode-ft8.sh $DIR/decode-ft8.sh.orig
 
+cp decode-ft8.sh $DIR
+
+lbu commit -d
