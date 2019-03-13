ba17842b35a47d0e99baa690ebf9591624d26b0a
root <root@rp-f05769.ekelund.nu>
2019-03-13 17:18:14 +0000

    Boot

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ cp upload-to-rbn $DIR
 # Back up original decode-ft8.sh 
 test -e $DIR/decode-ft8.sh.orig || \
 	cp $DIR/decode-ft8.sh $DIR/decode-ft8.sh.orig
-
 cp decode-ft8.sh $DIR
+
+# Set FT8 receiver to run at boot
+cp $DIR/start.sh /media/mmcblk0p1
 
 lbu commit -d
